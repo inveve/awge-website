ca8a252944c79d1f6053e3049e63aa956197451c
thinkgreensupplies <zachary@inveve.com>
2022-02-02 19:29:17 -0500

    content fix

--- scripts/email.php
+++ scripts/email.php
@@ -2,11 +2,11 @@
 
 $msg = "Email: " . $_GET['email'] . "\nSubject: " . $_GET['subject'] . "\nMessage: " . $_GET['message'] . "";
 $msg = wordwrap($msg,70);
-$subject = "AWGE Website Message :: " . $_GET['subject'] . "";
+$subject = "ARCANE Website Message :: " . $_GET['subject'] . "";
 
-$mail=mail("miniwandco@gmail.com", $subject, $msg, $header);
-$mail=mail("revengexstormtech@gmail.com", $subject, $msg, $header);
-$mail=mail("robert@awgeshit.com", $subject, $msg, $header);
-$mail=mail("support@SpecialDeliveryLA.com", $subject, $msg, $header);
+$mail=mail("zachary@inveve.com", $subject, $msg, $header);
+$mail=mail("zachary@inveve.com", $subject, $msg, $header);
+$mail=mail("zachary@inveve.com", $subject, $msg, $header);
+$mail=mail("zachary@inveve.com", $subject, $msg, $header);
 
 ?>
